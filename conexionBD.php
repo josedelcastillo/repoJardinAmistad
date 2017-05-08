dd705d122698c5e4a3e383fdd83ecc83776a500d
josedelcastillo <j.delcastillos@gmail.com>
2017-05-08 07:10:28 -0500

    f

--- conexionBD.php
+++ conexionBD.php
@@ -24,7 +24,6 @@ class BD {
 	$password = "";
 	$dbname = "marcaciones";
 	
-	
         $conn = new mysqli($servername, $username, $password,$dbname);
 		mysqli_set_charset(  $conn , 'utf8' );
         if ($conn->connect_error) {
